070cbcc30b8ba34301e9c112895478ac8aa86903
unknown <blessedadeleke@gmail.com>
2023-03-10 13:52:18 +0100

    update the serverurl

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
 
 
-const response = await fetch('http://localhost:5000', {
+const response = await fetch('https://chatai-e26g.onrender.com', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
